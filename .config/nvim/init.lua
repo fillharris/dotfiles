edd8fb0a2fd35fe70972a8d236b2de9b04acfbdc
Philip <ptharris79@gmail.com>
2022-06-27 17:16:48 -0400

    Add neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,20 @@
+
+local set = vim.opt
+
+-- Initial config
+set.autowrite = true
+set.autoindent = true
+set.tabstop = 2
+set.shiftwidth = 2
+set.softtabstop = 2
+set.expandtab = true
+set.cursorline = true
+set.number = true
+set.relativenumber = true
+set.mouse = "a"
+set.termguicolors = true
+set.hlsearch = true
+set.ignorecase = true
+set.smartcase = true
+set.signcolumn = "yes"
+set.clipboard = "unnamedplus"
